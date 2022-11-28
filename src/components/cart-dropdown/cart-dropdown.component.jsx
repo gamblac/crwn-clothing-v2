6a91ed651d7d8c5b1b025c1bbbf143213d9a970a
gamblac <ga129@student.london.ac.uk>
2022-11-28 21:16:17 +0100

    last commit

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -8,12 +9,17 @@ import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+  const goToCheckoutHandler = () => {
+    navigate('/checkout')
+  }
+  
   return (
     <div className="cart-dropdown-container">
     <div className="cart-items">
       {cartItems.map(item => <CartItem key={item.id} cartItem={item}></CartItem>)}
     </div>
-    <Button>GO TO CHECKOUT</Button>
+    <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
   </div>
   );
 };
