e1277b9e58631d729096e91df8c7e677f06acfed
gamblac <ga129@student.london.ac.uk>
2022-11-28 21:16:04 +0100

    last commit

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -37,7 +37,7 @@ const Navigation = () => {
           )}
           <CartIcon></CartIcon>
         </div>
-        {isCartOpen && <CartDropdown></CartDropdown>}
+        {isCartOpen && <CartDropdown></CartDropdown>} 
       </div>
       <Outlet />
     </Fragment>
